336266b689920c5d3d4f2d2af5e9326c5a0cca00
Zhongyue Ren <zhongyr@ada8.computing.clemson.edu>
2019-11-20 19:52:56 -0500

    Encapsulation function updataFromCache(_white_list)

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 from WhitList import MyWhiteList
 from python_arptable import get_arp_table
 
+def updateFromCache(_white_list):
+    current_arp_table = get_arp_table()
+    for entry in current_arp_table:
+         #print(entry["IP address"],entry["HW address"])
+         # validation
+         _white_list.update(entry["IP address"], entry["HW address"])
+    _white_list.write2file() 
+
+
 if __name__ == '__main__':
     white_list = MyWhiteList("text_list.txt")
     for ip, mac in white_list.data.items():
-        print(ip, mac)
+        print(ip,'\t', mac["time"],'\t', mac["mac"])
 
-    current_arp_table = get_arp_table()
-    for entry in current_arp_table:
-         print(entry["IP address"],entry["HW address"])
-         # validation
-         white_list.update(entry["IP address"], entry["HW address"])
-         white_list.write2file()
+    updateFromCache(white_list)
+
+
